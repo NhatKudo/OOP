2c3c0df70e32aaef3543f4818a9bc81eb3e7ed2a
devgooner <duynhatnguyenit@gmail.com>
2024-07-24 22:37:03 +0700

    Class and Object

--- oop.cpp
+++ oop.cpp
@@ -1,8 +1,13 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+class Employee{
+  std::string Name;
+  std::string Company;
+  int Age;
+};
 int main(int argc, char* argv[])
 {
-  std::cout<<" Hello World"<<std::endl;
+  Employee employee1;
   return 0;
 }
