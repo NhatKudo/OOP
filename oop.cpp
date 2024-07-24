82139f0633155a4da080b537539bc87925598e74
devgooner <duynhatnguyenit@gmail.com>
2024-07-24 23:13:46 +0700

    Access Modifiers

--- oop.cpp
+++ oop.cpp
@@ -2,12 +2,23 @@
 #include <iostream>
 #include <string>
 class Employee{
-  std::string Name;
-  std::string Company;
-  int Age;
+  public:
+    std::string Name;
+    std::string Company;
+    int Age;
+
+    void IntroduceYourself(){
+      std::cout<<"Name - "<<Name<<std::endl;
+      std::cout<<"Company - "<<Company<<std::endl;
+      std::cout<<"Age - "<<Age<<std::endl;
+    }
 };
 int main(int argc, char* argv[])
 {
   Employee employee1;
+  employee1.Name = "Kudo";
+  employee1.Company = "Movie JP";
+  employee1.Age = 24;
+  employee1.IntroduceYourself();
   return 0;
 }
