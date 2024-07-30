e19b6034d49a436bec007f09bf8f8ad2e705e6f7
devgooner <duynhatnguyenit@gmail.com>
2024-07-30 20:55:19 +0700

    Encapsulation

--- oop.cpp
+++ oop.cpp
@@ -2,15 +2,23 @@
 #include <iostream>
 #include <string>
 class Employee{
-  public:
+  private:
     std::string Name;
     std::string Company;
     int Age;
-
+  public:
     Employee(std::string name, std::string comapny, int age){
       Name = name;
       Company=comapny;
       Age=age;
+    }
+
+    void setName(std::string name){
+      Name=name;
+    }
+
+    std::string getName(){
+      return Name;  
     }
 
     void IntroduceYourself(){
@@ -23,5 +31,9 @@ int main(int argc, char* argv[])
 {
   Employee employee1 = Employee("Kudo", "JP-Movie", 25);
   employee1.IntroduceYourself();
+  employee1.setName("Heji");
+  employee1.IntroduceYourself();
+  std::string nameEmployee1 = employee1.getName();
+  std::cout<<"name of employee1: "<<nameEmployee1<<std::endl;
   return 0;
 }
